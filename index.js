1462145dcfcad16fc5732ac22dc7b52d5a1376ba
digitalfrost <gerald.hofmaier@gmail.com>
2016-10-22 18:29:13 +0100

    test solution

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ function authenticate(socket, data, callback) {
     console.log("Socket: "+ socket.id);
 
    //assume client sent correct credentials and authenticate...
-   return callback(null, data);
+   //return callback(null, data);
 
+    //assume client sent incorrect credentials and send error
+    return callback(new Error("Nobody is getting authenticated"));
 }
 
 function postAuthenticate(socket, data) {
@@ -27,7 +29,11 @@ function postAuthenticate(socket, data) {
 
 }
 
-
+//Clients should only receive this if authenticated
+setTimeout(function(){
+    io.emit("message", "Sending to Authenticated Users");
+    console.log("Sending stuff to authenticated users!")
+},10000);
 
 app.get('/', function(req, res){
     res.sendfile('index.html');
